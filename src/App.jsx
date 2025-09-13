f5da7c05bb2a9235e1f2abdb721f1309f68c07ee
HK <huseyinavnikadioglu@gmail.com>
2025-09-14 01:53:04 +0300

    seo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, useRef } from "react";
 import "./App.css";
-import { Helmet } from "react-helmet";
 
 import WORDS from "./data/turkish_words.json";
 
@@ -147,20 +146,6 @@ function App() {
 
   return (
     <div className="app">
-      <Helmet>
-        <title>⌨️ Typing Rush - Hızını Test Et!</title>
-        <meta
-          name="description"
-          content="En hızlı yazan sen misin? Typing Rush ile yazma hızını test et!"
-        />
-        <meta property="og:title" content="⌨️ Typing Rush - Hızını Test Et!" />
-        <meta
-          property="og:description"
-          content="En hızlı yazan sen misin? Typing Rush ile öğren!"
-        />
-        <meta property="og:image" content="oyun_ekran_gorseli.png" />
-        <meta property="og:url" content="https://seninsiten.com" />
-      </Helmet>
       <h1 className="game-title">Klavye Hız Testi</h1>
 
       {!gameStarted && (
